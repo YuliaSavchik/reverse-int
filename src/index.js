a8e4898a73c5da1c3840623e67a85d7b462d2054
YS <yulia.deamchik@gmail.com>
2021-08-10 17:30:00 +0300

    init: add answer task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
 module.exports = function reverse (n) {
-  
+    let num = Math.abs(n)
+    let str = String(num) 
+    let arr = str.split('')
+    let res = arr.reverse().join('')
+    return Number(res)
 }
